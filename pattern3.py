c521ca74daaae753c9e56510be7b7549b5bd0f14
Almas-Akhter <160632064+Almas-Akhter@users.noreply.github.com>
2024-03-01 09:11:44 +0530

    Update pattern3.py

--- pattern3.py
+++ pattern3.py
@@ -1,6 +1,6 @@
 rows = 5
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for i in range(rows):
-    for j in range(0, i + 1):
-        print(alpha[i], end=" ")
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print(alpha[j], end=" ")
     print()
